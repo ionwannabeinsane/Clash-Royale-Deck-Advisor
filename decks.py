4dc53265bb9d98e0e0e0849f02440d201d784026
ionwannabeinsane <leiflogan@icloud.com>
2025-06-18 13:20:15 -0500

    Add files via upload

---
+++ decks.py
@@ -0,0 +1,35 @@
+decks = [
+    {
+        "deck": ["Hog Rider", "Firecracker", "Ice Spirit", "Cannon", "Log", "Musketeer", "Skeletons", "Knight"],
+        "label": "good"
+    },
+    {
+        "deck": ["Golem", "Night Witch", "Baby Dragon", "Tornado", "Lightning", "Lumberjack", "Dark Prince", "Electro Dragon"],
+        "label": "good"
+    },
+    {
+        "deck": ["Goblin Giant", "P.E.K.K.A.", "Goblin Machine", "Mega Minion", "Goblin Curse", "Bomber", "Arrows", "Rage"]
+        "label": "good"
+    },
+    {
+        "deck": ["Goblin Giant", "Zap", "Sparky", "Rage", "Elite Barbarians", "Dark Prince", "Electro Wizard", "Heal Spirit"]
+        "label": "good"
+    },
+    {
+        "deck": ["Witch", "Knight", "Graveyard", "Poison", "Ice Wizard", "Goblin Hut", "Skeletons", "Barbarian Barrel"]
+        "label": "good"
+    },
+    {
+        "deck": ["Ice Spirit", "Heal Spirit", "Electro Spirit", "Skeletons", "Zap", "Ice Golem", "Inferno Spirit", "Spear Goblins"]
+        "label": "bad"
+    },
+    {
+        "deck": []
+        "label": ""
+    },
+]
+
+    
+
+    
+    
