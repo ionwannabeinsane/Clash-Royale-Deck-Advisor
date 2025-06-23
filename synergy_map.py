656c04a5e48ed55d43d8df1b5ae850d3379c940c
ionwannabeinsane <leiflogan@icloud.com>
2025-06-23 17:48:54 -0500

    Create synergy_map.py

---
+++ synergy_map.py
@@ -0,0 +1,52 @@
+synergy_map = {
+    # Miner Cycle
+    "Miner": ["Wall Breakers", "Poison", "Bats", "Knight", "Dark Prince", "Goblin Gang"],
+    "Wall Breakers": ["Miner", "Bats", "Fireball", "Knight"],
+
+    # Hog
+    "Hog Rider": ["Ice Spirit", "Earthquake", "Firecracker", "Valkyrie", "Cannon"],
+
+    # Royal Giant
+    "Royal Giant": ["Fisherman", "Phoenix", "Hunter", "Mother Witch", "Lightning"],
+    "Fisherman": ["Royal Giant", "Hunter", "Phoenix"],
+
+    # Balloon / LumberLoon
+    "Balloon": ["Miner", "Freeze", "Rage", "Baby Dragon", "Lumberjack", "Ice Golem"],
+    "Lumberjack": ["Balloon", "Golem", "Baby Dragon"],
+
+    # Graveyard
+    "Graveyard": ["Poison", "Baby Dragon", "Ice Wizard", "Knight", "Tornado", "Bowler"],
+
+    # X-Bow
+    "X-Bow": ["Tesla", "Knight", "Archers", "Ice Spirit", "Fireball", "Log"],
+
+    # Mortar Miner
+    "Mortar": ["Miner", "Bats", "Skeletons", "Ice Spirit", "Fireball"],
+
+    # Golem
+    "Golem": ["Night Witch", "Baby Dragon", "Lumberjack", "Phoenix", "Tornado", "Lightning"],
+    "Night Witch": ["Golem", "Lumberjack", "Baby Dragon"],
+
+    # Lava Hound
+    "Lava Hound": ["Balloon", "Miner", "Phoenix", "Tombstone", "Flying Machine", "Guards"],
+
+    # Electro Giant
+    "Electro Giant": ["Phoenix", "Electro Dragon", "Tornado", "Lightning", "Baby Dragon"],
+
+    # Goblin Giant + Sparky
+    "Goblin Giant": ["Sparky", "Electro Wizard", "Mini P.E.K.K.A", "Bats"],
+    "Sparky": ["Goblin Giant", "Mini P.E.K.K.A", "Zap"],
+
+    # Ram Rider / Bridge Spam
+    "Ram Rider": ["Bandit", "Royal Ghost", "Archer Queen", "Magic Archer", "P.E.K.K.A"],
+    "P.E.K.K.A": ["Battle Ram", "Bandit", "Royal Ghost", "Magic Archer", "Poison"],
+
+    # Prince / Double Prince
+    "Prince": ["Dark Prince", "Giant", "Miner", "Zap"],
+    "Dark Prince": ["Prince", "Giant", "Miner"],
+
+    # Spell pairings
+    "Tornado": ["Executioner", "Baby Dragon", "Valkyrie"],
+    "Freeze": ["Balloon", "Graveyard"],
+    "Poison": ["Miner", "Graveyard"],
+}
